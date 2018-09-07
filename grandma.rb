0d923aadbaaeee6b538a09f673d8573d74f3a49d
Amil Masic <amil.masic@gmail.com>
2018-09-07 01:05:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
   #   return "I LOVE YOU TOO, PUMPINK!"
   love = "i love you grandma"
   if phrase = love.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
