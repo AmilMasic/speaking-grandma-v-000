3769515ade3efafb6c698bfb329c495af77a9782
Amil Masic <amil.masic@gmail.com>
2018-09-07 01:08:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,22 +12,22 @@ require "pry"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
-  # if phrase != phrase.upcase
-  #   return "HUH?! SPEAK UP, SONNY!"
-  # elsif phrase = phrase.upcase
-  #   return "NO, NOT SINCE 1938!"
-  # else
-  #   phrase = "I LOVE YOU GRANDMA!"
-  #   return "I LOVE YOU TOO, PUMPINK!"
-  love = "i love you grandma"
-  if phrase = love.upcase
-  binding.pry
-    return "I LOVE YOU TOO PUMPKIN!"
-
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase = phrase.upcase
     return "NO, NOT SINCE 1938!"
-
   else
-    return "HUH?! SPEAK UP, SONNY!"
-  end
+    phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO, PUMPINK!"
+  # love = "i love you grandma"
+  # if phrase = love.upcase
+  # binding.pry
+  #   return "I LOVE YOU TOO PUMPKIN!"
+  # 
+  # elsif phrase = phrase.upcase
+  #   return "NO, NOT SINCE 1938!"
+  # 
+  # else
+  #   return "HUH?! SPEAK UP, SONNY!"
+  # end
 end
