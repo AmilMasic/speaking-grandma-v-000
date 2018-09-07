f61a3ebf638808219d7a7662e7295018709aadf2
Amil Masic <amil.masic@gmail.com>
2018-09-07 01:08:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -23,10 +23,10 @@ def speak_to_grandma(phrase)
   # if phrase = love.upcase
   # binding.pry
   #   return "I LOVE YOU TOO PUMPKIN!"
-  # 
+  #
   # elsif phrase = phrase.upcase
   #   return "NO, NOT SINCE 1938!"
-  # 
+  #
   # else
   #   return "HUH?! SPEAK UP, SONNY!"
   # end
