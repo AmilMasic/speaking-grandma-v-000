800f1b931e690ed074135e8433e6ac22316bfaea
Amil Masic <amil.masic@gmail.com>
2018-09-07 01:13:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@ require "pry"
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = phrase.upcase
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
   # love = "i love you grandma"
